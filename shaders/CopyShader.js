54ab50b386ac0e02dbf514836e45875129a94082
johnhuang <johnhuangx@outlook.com>
2019-01-02 19:25:30 +0800

    77777

---
+++ shaders/CopyShader.js
@@ -0,0 +1,34 @@
+THREE.CopyShader={
+    uniforms:{
+        "tDiffuse":{value:null},
+        "opacity":{value:1.0}
+    },
+    vertexShader:[
+
+        "varying vec2 vUv;",
+
+        "void main(){",
+
+            "vUv=uv",
+            "gl_Position = projectionMatrix * modelMatrix * vec4( position, 1.0);",
+
+        "}"
+    ].join("\n"),
+
+    fragmentShader:[
+
+        "uniform float opacity;",
+
+        "uniform sampler2D tDiffuse",
+
+        "varying vec2 vUv;",
+
+        "void main(){",
+
+            "vec4 texel = texture2D(tDiffuse, vUv);",
+            "gl_FragColor = opacity * texel",
+
+        "}"
+
+    ].join("\n")
+};
